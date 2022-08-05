c7e92c877be8842ee0cba35233aa88957e1caea6
lhb5185918 <104125540+lhb5185918@users.noreply.github.com>
2022-08-05 12:47:53 +0800

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 import pymysql
-db = pymysql.connect(host="10.32.176.2",
+db = pymysql.connect(host="101.43.155.83",
                      port=3306,
-                     user="select_user",
-                     password="select_user@321",
-                     database="lingxi_boot")
+                     user="root",
+                     password="123456",
+                     database="zrlog")
